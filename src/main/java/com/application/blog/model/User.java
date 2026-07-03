1196b8f270be629dd44c76dcddcbb985a3e1d549
gkrvkoushik <gkrvkoushik@gmail.com>
2026-07-03 11:08:19 +0530

    v0.1

--- src/main/java/com/application/blog/model/User.java
+++ src/main/java/com/application/blog/model/User.java
@@ -1,7 +1,26 @@
 package com.application.blog.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
+@Entity
+@Table(name="user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Email(message = "Provide proper email")
+    private String email;
+
+    @Min(value = 6)
+    private String password;
+
+    @OneToMany(fetch=FetchType.LAZY,mappedBy = "user")
+    private List<Blog> blogs;
 
 
 }
